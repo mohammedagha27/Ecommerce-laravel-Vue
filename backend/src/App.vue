7286e38a93304693548b8a9d0d48861f3ee549cc
Mohammed alagha <modyagha7@gmail.com>
2022-12-25 18:50:40 +0200

    add vuex and create store

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { computed } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import store from "./store";
+const test = computed(() => store.state.test);
 </script>
 
 <template>
     <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p class="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
+        Welcome! {{ test }}
       </p>
       <p class="text-gray-500 text-lg">
         Vue and Tailwind CSS in action
---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+        test: 'this is a test'
+    },
+    mutations: {},
+    getters: {},
+    actions: {},
+})
+
+export default store;
